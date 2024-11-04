69a32f4c62d5f1dc5ec280f25879853b6535e2dc
solayman-ds <semon@miners.utep.edu>
2024-11-04 16:22:26 -0700

    df

--- max-arr.c
+++ max-arr.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
 
 int main()
 
@@ -6,15 +9,46 @@ int main()
 
 
 
-    int dog[2] = {-90, 8};
+    int dog[] = {-90, 8};
+    it *ptr2dog = &dog[0];
+
+    int size = sizeof(dog) / sizeof(dog[0]);
+
+
+
+    printf("%d", size);
+
+    int arr[] = {19,25,10,3,1,8,22,17,7,84,9,19,25,10,3,1,
+             8,22,17,7,84, 9,19,25,10,3,1,8,22,17,7,84,
+             9,19,25,10,3,1,8,22,17,7,84,9,19,25,10,33,
+             1,8,22,17,7,84,9,19,25,10,3,1,8,22,17,7,84,
+             9,19,25,10,3,1,8,22,17,7,84,9,11,23,45,5,3};
+
+    //using sizeof() operator to get length of array
+    //printf(" The length of int array is : %ld ", sizeof(arr) / sizeof(arr[0]));
+
+
+   
 
     int max = dog[0];
 
-    if (max > dog[1])
+   /*if (max > dog[1])
         printf("the max is %d\n", max);
 
     else
     {
         printf("the max is %d\n", dog[1]);
+    } */
+
+
+    for (int i = 0; i <size; ++i){
+
+        if (*ptr2dog > *(ptr2dog+1)){
+           max = *ptr2dog;
+        }
+
+        else{
+
+        }
     }
 }
